d486e67613e99d6a3e8c424efd6652676162a14a
Karthikeya gaddam <karthikeyagaddam257@gmail.com>
2025-12-09 01:50:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@
 
 # app.add_middleware(
 #     CORSMiddleware,
-#     allow_origins=["https://fubotics-software-ai-assignment-six.vercel.app"],
+#     allow_origins=["https://fubotics-software-ai-assignment-six.vercel.app", "http://localhost:5173"],
 #     allow_credentials=True,
 #     allow_methods=["*"],
 #     allow_headers=["*"],
